236739e069db5e41295cca45b2d1374a0306f0d6
Abraham Gutierrez <labraham1986@gmail.com>
2024-04-11 22:03:29 -0500

    feat: add json web token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,16 +4,19 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcryptjs from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 import { User } from './entities/user.entity';
 import { LoginDto } from './dto/login.dto';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
 
   constructor(
     @InjectModel(User.name)
-    private userModel: Model<User>
+    private userModel: Model<User>,
+    private jwtService: JwtService
   ) { }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -72,7 +75,7 @@ export class AuthService {
 
     return {
       user: rest,
-      token: 'ABC-123'
+      token: this.getJwtToken({ id:user.id }),
     }
 
     /** JWT
@@ -96,5 +99,11 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken( payload: JwtPayload){
+
+    const token = this.jwtService.sign( payload );
+    return token;
   }
 }
